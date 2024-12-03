762e489357a682cddde8b758265358fc421c068a
leifhauskendev <leifhauskendev@gmail.com>
2024-12-03 17:13:34 -0600

    #18 - created initial front end for strategy screen

---
+++ Pages/Stratagies/StrategyScreen/StrategyScreenVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategySync
+{
+    public class StrategyScreenVM : INotifyPropertyChanged
+    {
+        public StrategyScreenVM() { 
+
+        }
+
+        private string _user;
+
+        public string User
+        {
+            get { return _user; }
+            set
+            {
+                if (_user != value)
+                {
+                    _user = value;
+                    OnPropertyChanged(nameof(_user));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
